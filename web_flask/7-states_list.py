114079601c40cf5531ea4ba75d2bb94a944d6e8a
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2021-09-12 20:05:59 +0100

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,61 +10,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hbnb():
-    """
-        fn to display Hello HBNB!
-        in the route page
-    """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb_():
-    """
-        fn to display HBNB!
-        in the route page
-    """
-    return 'HBNB!'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def hbnb_c(text):
-    """
-        fn to display a text
-        in the route page
-    """
-    txt = text.replace('_', ' ')
-    return 'C {}'.format(txt)
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def hbnb_n(n):
-    """
-        fn to display an integer n
-        in the route page
-    """
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def hbnb_html(n):
-    """
-        fn to display display a
-        HTML page only if n is an integer
-    """
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def hbnb_odd_even(n):
-    """
-        fn to display display a
-        HTML page only if n is an integer
-    """
-    return render_template('6-number_odd_or_even.html', n=n)
-
-
 @app.route('/state_list', strict_slashes=False)
 def hbnb_state_list():
     """
